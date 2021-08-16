4cffdc9e41fbaa2868f1126a800a784cc72190a2
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-08-16 12:22:15 +0200

    Create basic express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello Server');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!');
+});
+
+app.listen(3000, () => {
+  console.log(`Server is listening on port ${port}`);
+});
